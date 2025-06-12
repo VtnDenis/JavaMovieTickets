c0d768aca33e13cc43e014a13b78951b2864c5c6
frankigoes6 <frank.braud@hotmail.com>
2025-06-12 14:24:30 +0200

    connexion a la base de donnée

---
+++ src/Mysqlc.java
@@ -0,0 +1,40 @@
+import java.sql.*;
+
+/**
+ *
+ * @author mysqltest
+ */
+public class Mysqlc {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Connection conn = null;
+        try {
+            // db parameters - ptest is the name of the database
+            String url       = "jdbc:mysql://localhost:3306/cinema";
+            String user      = "root";
+            String password  = "root";
+
+            // create a connection to the database
+            conn = DriverManager.getConnection(url, user, password);
+            // more processing here
+            // ...
+            Statement stmt=conn.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from booking");
+            while(rs.next())
+                System.out.println(rs.getInt(1)+"  "+rs.getString(2));
+
+        } catch(SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try{
+                if(conn != null)
+                    conn.close();
+            }catch(SQLException ex){
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+}
